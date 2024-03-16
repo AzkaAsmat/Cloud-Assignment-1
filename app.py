dfb8e9568414ec2c92881524e011f4ec65a0eca5
T A L I K A <159610428+Talikamuhib@users.noreply.github.com>
2024-03-16 15:58:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ THIS CODE FILE CONTAINS THE CODE FOR MAIN APP OF THE WEB STORAGE APP.
 
 THIS FILE DEFINES A main() FUNCTION THAT WILL BE EXECUTED WHEN THE APP RUNS. INSIDE THIS FUNTION ALL THE OTHER MENU TABS OF THE APP IS CALLED.
 """
+import subprocess
 
+# Define the list of packages to install
+packages = ["matplotlib", "mysql-connector-python", "sqlalchemy"]
+
+# Use pip to install each package
+for package in packages:
+    try:
+        subprocess.check_call(["pip", "install", package])
+        print(f"{package} has been successfully installed.")
+    except subprocess.CalledProcessError:
+        print(f"Error: Failed to install {package}.")
 
 from dependancies import *
 from home import *
